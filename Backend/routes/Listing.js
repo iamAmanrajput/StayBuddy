eaf01a14917d7bd71137b70e66da93fc13fff612
Aman Singh <aman.it360@gmail.com>
2024-11-24 14:18:50 +0530

    Created getOneListing Route

--- Backend/routes/Listing.js
+++ Backend/routes/Listing.js
@@ -6,11 +6,13 @@ const {
   updateListing,
   deleteListing,
   getAllListings,
+  getOneListing,
 } = require("../controllers/Listing");
 
 router.post("/new", auth, createListing);
 router.post("/update/:id", auth, updateListing);
 router.delete("/delete/:id", auth, deleteListing);
 router.get("/", getAllListings);
+router.get("/:id", getOneListing);
 
 module.exports = router;

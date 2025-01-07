428252e37c474b439434e8bd8af4c0dee2208229
Aman Kumar <aman.it360@gmail.com>
2025-01-07 12:34:22 +0530

    Update User.js

--- Backend/routes/User.js
+++ Backend/routes/User.js
@@ -4,6 +4,6 @@ const { auth } = require("../middlewares/Auth");
 const { signup, login, logout } = require("../controllers/Auth");
 
 router.post("/signup", signup);
-router.post("/login", login);
+router.post("/login", login); 
 router.get("/logout", auth, logout);
 module.exports = router;
